ba6e8e7bfe60ff0991900cbdd0ad3beb713140ea
Roman <Sinigr38@mail.ru>
2015-04-12 21:09:54 +0800

    Immersive mode для устройств 4.4+

--- android/src/com/sinigr/arca/android/AndroidLauncher.java
+++ android/src/com/sinigr/arca/android/AndroidLauncher.java
@@ -1,6 +1,9 @@
 package com.sinigr.arca.android;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -12,5 +15,19 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new ScreenController(), config);
+	}
+	// Immersive mode
+	@TargetApi(19)
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if(Build.VERSION.SDK_INT >= 19) {
+			if(hasFocus) getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
 	}
 }
